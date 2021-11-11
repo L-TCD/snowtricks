53a029266ddd0a1431456f53ccd9ecb878c34297
L-TCD <loic.coissard@gmail.com>
2021-11-11 12:17:06 +0100

    add trick fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,47 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Trick;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+	protected $slugger;
+	protected $encoder;
+
+	public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $encoder)
+	{
+		$this->slugger = $slugger;
+		$this->encoder = $encoder;
+	}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+		$faker = Factory::create('fr_FR');
+
+		for ($t = 0; $t < 20; $t++) {
+			$trick = new Trick;
+			$trick
+				->setName(ucfirst($faker->words(mt_rand(2, 4), true)))
+				->setSlug(strtolower($this->slugger->slug($trick->getName())))
+				->setCreatedAt($faker->dateTimeBetween('-3 weeks'));
+
+                $random = mt_rand(0, 3);
+                if ($random >= 1) {
+					$trick->setDescription($faker->paragraphs(mt_rand(1, 3), true));
+				}
+
+                $random2 = mt_rand(0, 3);
+                if ($random2 < 1) {
+					$trick->setUpdateAt($faker->dateTimeBetween($trick->getCreatedAt()));
+                }
+
+	        $manager->persist($trick);
+		}
 
         $manager->flush();
     }
---
+++ src/Entity/Trick.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrickRepository::class)
+ */
+class Trick
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updateAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTime
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(?\DateTime $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+}
